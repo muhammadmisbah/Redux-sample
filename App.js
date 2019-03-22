2d3ee2ff7214ff2dcca3502277c25f541085d74b
Muhammad Misbah <muhammadmisbah107@gmail.com>
2019-03-22 22:21:14 +0500

    changed style of buttons

--- App.js
+++ App.js
@@ -7,18 +7,18 @@ class App extends Component {
   render() {
     const text = this.props.Text, Buttons = this.props.Buttons;
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: text.toLowerCase(), }}>
         <View style={{ flex: 0.8, justifyContent: "center", alignItems: "center" }}>
-          <View style={{ padding: 20, backgroundColor: text.toLowerCase(), }}>
+          <View style={{ padding: text ? 5 : 0, backgroundColor: "#000", }}>
             <Text style={{ color: "#fff" }}>{text}</Text>
           </View>
         </View>
-        <View style={{ flex: 0.2, width: "100%", flexDirection: "row", alignItems: "center", justifyContent: 'space-evenly' }}>
+        <View style={{ flex: 0.2, width: "100%", flexDirection: "row", alignContent: "space-around", justifyContent: 'space-evenly', flexWrap: "wrap" }}>
           {Buttons.map((item, index) => (
             <TouchableOpacity
               key={index}
               onPress={() => { this.props.ChangeText(item) }}
-              style={{ justifyContent: 'center', alignItems: 'center', width: "22%", height: "90%", backgroundColor: "black", borderRadius: 5 }}
+              style={{ justifyContent: 'center', alignItems: 'center', width: "46%", height: "42%", backgroundColor: "black", borderRadius: 5 }}
             >
               <Text style={{ color: "#fff" }}>{item}</Text>
             </TouchableOpacity>
