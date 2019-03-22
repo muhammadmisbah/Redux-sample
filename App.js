685a4e9d6197afa4db40de8c01ee3fa15feaeb91
Muhammad Misbah <muhammadmisbah107@gmail.com>
2019-03-22 22:42:42 +0500

    added scrollview

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { connect } from "react-redux"
 import { MainMiddleware } from './src/store/middlewares';
 
@@ -14,15 +14,19 @@ class App extends Component {
           </View>
         </View>
         <View style={{ flex: 0.2, width: "100%", flexDirection: "row", alignContent: "space-around", justifyContent: 'space-evenly', flexWrap: "wrap" }}>
-          {Buttons.map((item, index) => (
-            <TouchableOpacity
-              key={index}
-              onPress={() => { this.props.ChangeText(item) }}
-              style={{ justifyContent: 'center', alignItems: 'center', width: "46%", height: "42%", backgroundColor: "black", borderRadius: 5 }}
-            >
-              <Text style={{ color: "#fff" }}>{item}</Text>
-            </TouchableOpacity>
-          ))}
+          <ScrollView>
+            <View style={{ width: "100%", flexDirection: "row", alignContent: "space-around", justifyContent: 'space-evenly', flexWrap: "wrap" }}>
+              {Buttons.map((item, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => { this.props.ChangeText(item) }}
+                  style={{ justifyContent: 'center', alignItems: 'center', width: "46%", height: 50, marginVertical: 5, backgroundColor: "black", borderRadius: 5 }}
+                >
+                  <Text style={{ color: "#fff" }}>{item}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
         </View>
       </View>
     );
